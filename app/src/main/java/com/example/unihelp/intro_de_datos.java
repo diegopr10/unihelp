555fdc41d14d94fcc5bb311b596f7764546e7d93
Rubo120 <100363766@alumnos.uc3m.es>
2023-03-17 11:44:06 +0100

    He metido la logica para la actividad de introduccion de datos(intro_de_datos.java)

--- app/src/main/java/com/example/unihelp/intro_de_datos.java
+++ app/src/main/java/com/example/unihelp/intro_de_datos.java
@@ -2,13 +2,75 @@ package com.example.unihelp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class intro_de_datos extends AppCompatActivity {
+
+    private Spinner spinner1,spinner2,spinner3;
+    private EditText et1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro_de_datos);
+
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        spinner3 = (Spinner) findViewById(R.id.spinner3);
+        et1 = (EditText)findViewById(R.id.campo_nombre);
+
+        String [] opciones1 = {"Sonido e Imagen","Teleco","Telemática","Espaciales"};
+        String [] opciones2 = {"Primero.","Segundo.","Tercero.","Cuarto"};
+        String [] opciones3 = {"Español","Inglés"};
+
+
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, opciones1);
+        spinner1.setAdapter(adapter1);
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, opciones2);
+        spinner2.setAdapter(adapter2);
+
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, opciones3);
+        spinner3.setAdapter(adapter3);
+
+
+
+    }
+
+    public void siguiente(View view){
+        String[] cadena = {"alias","carrera","curso","idioma"};
+
+        String valor1_string = et1.getText().toString();
+        String seleccion1 = spinner1.getSelectedItem().toString();
+        String seleccion2 = spinner2.getSelectedItem().toString();
+        String seleccion3 = spinner3.getSelectedItem().toString();
+
+        SharedPreferences preferencias = getSharedPreferences("Preferencias", Context.MODE_PRIVATE);
+        SharedPreferences.Editor obj_editor = preferencias.edit();
+        obj_editor.putString(cadena[0],valor1_string);
+        obj_editor.commit();
+
+        obj_editor.putString(cadena[1],seleccion1);
+        obj_editor.commit();
+
+        obj_editor.putString(cadena[2],seleccion2);
+        obj_editor.commit();
+
+        obj_editor.putString(cadena[3],seleccion3);
+        obj_editor.commit();
+
+
+
+        Intent empezar = new Intent(this,inicio.class);
+        startActivity(empezar);
+
     }
 }
