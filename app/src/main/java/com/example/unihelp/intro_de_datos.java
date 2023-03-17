b55a7c212e76ad00de8e8e3de4a0ab6d942c6b66
Rubo120 <100363766@alumnos.uc3m.es>
2023-03-17 11:31:57 +0100

    He metido la pantalla de intro de datos (falta enlazar la primera pantalla de diego con esta segunda actividad, a parte hay que matizar el actionbar

---
+++ app/src/main/java/com/example/unihelp/intro_de_datos.java
@@ -0,0 +1,14 @@
+package com.example.unihelp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class intro_de_datos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_de_datos);
+    }
+}
