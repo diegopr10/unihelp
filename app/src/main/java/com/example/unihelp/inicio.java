393ef02dc1efac3dc8307c0f77a5575e6134b704
Rubo120 <100363766@alumnos.uc3m.es>
2023-03-17 11:54:26 +0100

    Ultimo commit basto en el que meto la tercera actividad con su codigo java

---
+++ app/src/main/java/com/example/unihelp/inicio.java
@@ -0,0 +1,26 @@
+package com.example.unihelp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class inicio extends AppCompatActivity {
+
+    private TextView alias;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+
+        alias = (TextView) findViewById(R.id.alias);
+
+        SharedPreferences preferencias = getSharedPreferences("Preferencias", Context.MODE_PRIVATE);
+        String cadena_alias = preferencias.getString("alias","");
+
+        alias.setText(cadena_alias);
+    }
+}
