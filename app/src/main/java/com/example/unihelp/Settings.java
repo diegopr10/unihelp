697f41a02d94f35303e961bf998cd2878f5e8d40
Diego Perez <100406217@alumnos.uc3m.es>
2023-03-21 11:59:39 +0100

    he metido un spinner en ajustes

--- app/src/main/java/com/example/unihelp/Settings.java
+++ app/src/main/java/com/example/unihelp/Settings.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class Settings extends AppCompatActivity {
@@ -13,6 +15,11 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         setTitle("Settings");
+
+        String[] items = {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        Spinner spinner_prueba = findViewById(R.id.spinner_prueba);
+        spinner_prueba.setAdapter(adapter);
     }
 
     public void goToHome(){
