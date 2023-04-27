3e88db7c2844fe1fe7221f92c466ee8e9f1b0c7a
Diego Perez <100406217@alumnos.uc3m.es>
2023-04-27 12:29:50 +0200

    Continuación calificaciones y eliminados fragmentos raros

--- app/src/main/java/com/example/unihelp/NoteCreator.java
+++ app/src/main/java/com/example/unihelp/NoteCreator.java
@@ -2,7 +2,13 @@ package com.example.unihelp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class NoteCreator extends AppCompatActivity {
 
@@ -10,5 +16,18 @@ public class NoteCreator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_creator);
+
+        EditText note_title = findViewById(R.id.note_title);
+        EditText note_subject = findViewById(R.id.note_subject);
+        EditText note_number = findViewById(R.id.note_number);
+        EditText note_percentage = findViewById(R.id.note_percentage);
+        Button add_note_button = findViewById(R.id.add_note_button);
     }
+
+    public void goToMarks(View view) {
+        Intent intent = new Intent(this, Marks.class);
+        startActivity(intent);
+    }
+
+
 }
