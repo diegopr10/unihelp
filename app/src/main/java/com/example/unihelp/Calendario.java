0e6779d9741dc863b0512cc61c0d3ce377281599
JoshAlberto <josh.trabajos@gmail.com>
2023-05-04 20:14:56 +0200

    Calendario

--- app/src/main/java/com/example/unihelp/Calendario.java
+++ app/src/main/java/com/example/unihelp/Calendario.java
@@ -3,8 +3,12 @@ package com.example.unihelp;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.Calendar;
 
@@ -12,32 +16,44 @@ import java.util.Calendar;
 public class Calendario extends AppCompatActivity {
 
     private TextView tv_Calendar;
+    private CalendarView cal;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendario);
 
         tv_Calendar= findViewById(R.id.tv_Calendar);
+        cal = findViewById(R.id.calendarView);
 
-
-    }
-
-    public void abrirCalendario(View view){
-
-        Calendar cal = Calendar.getInstance();
-        int year= cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH);
-        int day = cal.get(Calendar.DAY_OF_MONTH);
-
-        DatePickerDialog dialog= new DatePickerDialog(Calendario.this, new DatePickerDialog.OnDateSetListener() {
-
-            public void onDateSet(DatePicker datePicker, int anio, int mes, int dia) {
-                String fecha = dia + "/"+ mes+"/"+anio;
-                tv_Calendar.setText(fecha);
+        cal.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+                String fecha = day + "/"+ (month+1)+"/"+year;
+                Toast.makeText(Calendario.this,fecha, Toast.LENGTH_LONG).show();
             }
-        },day,month,year);
+        });
 
-        dialog.show();
 
     }
+
+//   Funcion para abrir un calendario como dialog en otra pestaña.
+
+//    public void abrirCalendario(View view){
+//
+//
+////        Calendar cal = Calendar.getInstance();
+////        int year= cal.get(Calendar.YEAR);
+////        int month = cal.get(Calendar.MONTH);
+////        int day = cal.get(Calendar.DAY_OF_MONTH);
+////
+////        DatePickerDialog dialog= new DatePickerDialog(Calendario.this, new DatePickerDialog.OnDateSetListener() {
+////
+////            public void onDateSet(DatePicker datePicker, int anio, int mes, int dia) {
+////                String fecha = dia + "/"+ mes+"/"+anio;
+////                tv_Calendar.setText(fecha);
+////            }
+////        },day,month,year);
+////
+////        dialog.show();
+//
+//    }
 }
