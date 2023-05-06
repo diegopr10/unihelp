b18349dba331f82976e93e9ce1bf72484406358a
JoshAlberto <josh.trabajos@gmail.com>
2023-05-06 06:55:32 +0200

    Modo estudio ubicacion

--- app/src/main/java/com/example/unihelp/location.java
+++ app/src/main/java/com/example/unihelp/location.java
@@ -2,6 +2,8 @@ package com.example.unihelp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -27,9 +29,14 @@ public class location extends AppCompatActivity implements OnMapReadyCallback {
     }
 
     private void createMarker() {
-        LatLng coordinates = new LatLng(40.332306, -3.765627);
-        MarkerOptions marker = new MarkerOptions().position(coordinates).title("UC3M");
+        LatLng Uc3mLocation = new LatLng(40.332306, -3.765627);
+        MarkerOptions marker = new MarkerOptions().position(Uc3mLocation).title("UC3M");
         map.addMarker(marker);
+        map.animateCamera(
+                CameraUpdateFactory.newLatLngZoom(Uc3mLocation, 17f),
+                4000,
+                null
+        );
     }
     private void createFragment() {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentMap);
